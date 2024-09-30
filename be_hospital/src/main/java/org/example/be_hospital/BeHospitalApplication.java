545af08136b282e006a9a8cc4bf079a97d2f4618
bpbn <bpbngoc11@gmail.com>
2024-09-30 11:07:18 +0700

    create project

---
+++ be_hospital/src/main/java/org/example/be_hospital/BeHospitalApplication.java
@@ -0,0 +1,13 @@
+package org.example.be_hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeHospitalApplication.class, args);
+	}
+
+}
---
+++ be_hospital/src/test/java/org/example/be_hospital/BeHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.be_hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
